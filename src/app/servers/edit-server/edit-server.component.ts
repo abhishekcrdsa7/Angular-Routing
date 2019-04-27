19e5c213a6081cd3db29b61f00a35c796f487a4a
abhishek sharma <abhishekcrdsa7@gmail.com>
2019-04-28 00:02:05 +0530

    Implementation of routing in angular

---
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+
+import { ServersService } from '../servers.service';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css']
+})
+export class EditServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+  serverName = '';
+  serverStatus = '';
+  allowedEdit: boolean = false;
+
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+    this.route.queryParams
+    .subscribe(
+      (query: Params) => {
+        this.allowedEdit = query.allowEdit == 1 ? true : false;
+      }
+    );
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+  }
+
+}
